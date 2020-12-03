56ae9d3b942adbad21b9afce655a95bb02d66b82
Killer117 <cooldeepakchopra@gmail.com>
2020-12-03 19:14:13 +0530

    organiser backend for registeration done

--- backend/models/Organiser.js
+++ backend/models/Organiser.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
+const Event = require("./Event");
 
 const organisersSchema = new mongoose.Schema({
   company_name: String,
   email: String,
-  contact_no: Number,
-  website_link: String,
+  address:String,
+  password:String,
+  mobile_no:Number,
+  landline_no:Number,
+  state:String,
+  city:String,
+  pincode:String,
   events: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
 });
 
--- backend/routes/organisers.js
+++ backend/routes/organisers.js
@@ -4,23 +4,28 @@ const router = express.Router();
 const Organiser = require("../models/Organiser");
 
 router.post("/register", function (req, res) {
-  User.find({ email: req.body.email }, function (err, foundOrganisers) {
+  Organiser.find({ email: req.body.email }, function (err, foundOrganisers) {
     if (foundOrganisers.length === 0) {
       const newOrganiser = new Organiser({
         company_name: req.body.name,
         email: req.body.email,
-        contact_no: req.body.mobile,
-        website_link: req.body.url,
+        address:req.body.address,
+        password:req.body.password,
+        mobile_no:req.body.mobile,
+        landline_no:req.body.landline,
+        state:req.body.state,
+        city:req.body.city,
+        pincode:req.body.pincode
       });
 
       newOrganiser.save();
       res.send(true);
+
     } else {
       res.send("Organiser already registered");
     }
   });
 });
 
-router.post("/login", function (req, res) {});
 
 module.exports = router;
